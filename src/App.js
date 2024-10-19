16edff646d417f0f92d94d833b2795c59f5bc4cf
Ivan Ćaleta <73293777+IvanCaleta@users.noreply.github.com>
2024-10-19 23:52:24 +0200

    SUBTASK-1: Setup App, Pages and Components (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HomePage, MovieDetailsPage, PlayScreenPage, SplashPage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/details" element={<MovieDetailsPage />} />
+          <Route path="/play" element={<PlayScreenPage />} />
+          <Route path="*" element={<SplashPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
