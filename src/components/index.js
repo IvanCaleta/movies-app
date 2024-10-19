5abafa920bbb9bae358ad6b3f287ed0085fd9a1c
ivan <icalet00@fesb.hr>
2024-10-19 23:42:57 +0200

    SUBTASK-1: Setup App, Pages and Components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import HomeView from "./HomeView/HomeView";
+import MovieDetailsView from "./MovieDetailsView/MovieDetailsView";
+import PlayView from "./PlayView/PlayView";
+
+export{
+    HomeView,
+    MovieDetailsView,
+    PlayView,
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import HomePage from "./HomePage";
+import MovieDetailsPage from "./MovieDetailsPage";
+import PlayScreenPage from "./PlayScreenPage";
+import SplashPage from "./SplashPage";
+
+export{
+    HomePage,
+    MovieDetailsPage,
+    PlayScreenPage,
+    SplashPage,
+}
