5d5603e0eb6b0592e9ad467f74298b4d8dac1b53
Ivan Ćaleta <73293777+IvanCaleta@users.noreply.github.com>
2024-10-20 22:46:23 +0200

    SUBTASK-3: Genres View (#3)

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import HomeView from "./HomeView/HomeView";
 import MovieDetailsView from "./MovieDetailsView/MovieDetailsView";
 import PlayView from "./PlayView/PlayView";
+import GenreRails from "./GenreRails/GenreRails";
+import MovieList from "./MovieList/MovieList";
 
 export{
     HomeView,
     MovieDetailsView,
     PlayView,
+    GenreRails,
+    MovieList,
 }
